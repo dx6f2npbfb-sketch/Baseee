f9212c3cf9feebfb7cde619728545c924d6959ea
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-20 21:06:19 -0500

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -2,7 +2,6 @@
 const handler = async (m, { conn }) => {
     const user = global.db.data.users[m.sender];
         conn.sendMessage(m.chat, {text: `*${emojis} @${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
-      global.db.data.users[m.sender].money = Infinity;
     global.db.data.users[m.sender].coins = Infinity;
   global.db.data.users[m.sender].level = Infinity;
  global.db.data.users[m.sender].exp = Infinity;
@@ -10,6 +9,6 @@ const handler = async (m, { conn }) => {
 handler.help = ['cheat'];
 handler.tags = ['owner'];
 handler.command = /^(ilimitado|infiniy|chetar)$/i;
-handler.mods = true;
+handler.rowner = true;
 handler.fail = null;
 export default handler;

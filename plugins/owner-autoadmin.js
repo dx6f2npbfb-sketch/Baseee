600fce31fc9382cc86fb8691315bde3b59d3804f
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-26 16:23:08 -0500

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -13,7 +13,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
 handler.tags = ['owner'];
 handler.help = ['autoadmin'];
 handler.command = ['autoadmin'];
-handler.mods = true;
+handler.owner = true;
 handler.group = true;
 handler.botAdmin = true;
 export default handler;

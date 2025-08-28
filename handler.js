82340ad3c04032dcd5bec01aaec001ae052ca516
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-27 19:53:48 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -195,8 +195,7 @@ export async function handler(chatUpdate) {
             console.error(e)
         }
 
-let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
-
+let _user = global.db.data?.users?.[m.sender]  // ✅ Solo una vez
 const detectwhat = m.sender.includes('@lid') ? '@lid' : '@s.whatsapp.net';
 const isROwner = [...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + detectwhat).includes(m.sender)
 const isOwner = isROwner || m.fromMe

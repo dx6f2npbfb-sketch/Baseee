3aeb41331603d7273131a5ea88e9a875a66239ec
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-09-01 21:42:01 -0500

    Actualizar plugins

---
+++ plugins/owner-addowner.js
@@ -0,0 +1,43 @@
+Coloca esto en el repo de jota
+
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+
+  const why = `*${emojis} Menciona al usuario para agregar o quitar owner.*`;
+  const who = m.mentionedJid[0] 
+    ? m.mentionedJid[0] 
+    : m.quoted 
+      ? m.quoted.sender 
+      : text 
+        ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' 
+        : false;
+  
+  if (!who) return conn.reply(m.chat, why, m, { mentions: [m.sender] });
+
+  switch (command) {
+    case 'addowner':
+      if (global.owner.some(o => o[0] === who)) {
+        return conn.reply(m.chat, `*⚠️ El número ya es owner.*`, m);
+      }
+      global.owner.push([who]);
+      await conn.reply(m.chat, `*✅ Listo, el usuario ya es owner.*`, m);
+      break;
+
+    case 'delowner':
+      const index = global.owner.findIndex(o => o[0] === who);
+      if (index !== -1) {
+        global.owner.splice(index, 1);
+        await conn.reply(m.chat, `*✅ Número eliminado de la lista de owners.*`, m);
+      } else {
+        await conn.reply(m.chat, `*⚠️ El número no está en la lista de owners.*`, m);
+      }
+      break;
+  }
+};
+
+
+handler.help = ['addowner', 'delowner'];
+handler.tags = ['owner'];
+handler.command = ['addowner', 'delowner'];
+handler.rowner = true;
+
+export default handler;

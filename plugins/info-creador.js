ccd29153033be6bc8322eafaffb3dc602beeee21
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 15:08:13 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
   let list = [
     {
       displayName: "Chinchu dzn",
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Chinchu dzn \nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem4.ADR:;;Argentina 🇦🇷;;;; },
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Chinchu\nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;; Argentina 🇦🇷;;;;\nitem4.X-ABLabel:País\nEND:VCARD` },
     {
       displayName: "Jota",
       vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Colombia 🇨🇴;;;;\nitem4.X-ABLabel:País\nEND:VCARD`

be49ced42725f6c949d53683a5eb7848a9a28896
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 18:02:59 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.creator = [
    ['573155227977', 'Jota 🐼', true]
 ]
 
-global.mods = ['51990841568', '51965911060', '51906168999']
+global.mods = ['51990841568', '51965911060']
 global.prems = ['51965911060', '51906168999']
 
 
@@ -51,7 +51,7 @@ global.channel = 'https://whatsapp.com/channel/0029VauTE8AHltY1muYir31n'
 global.ig = 'https://www.instagram.com/dev.criss_vx'
 
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝖲𝗁⍺𝖽ᦅ𝗐′𝗌  乂  𝖢𝗅𝗎𝖻', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'Yanito Bot MD', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 
 global.cheerio = cheerio

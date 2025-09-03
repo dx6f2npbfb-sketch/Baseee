8c7c99a8a6e16d96c7f82ab689f0924462419ea4
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 15:05:21 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -5,9 +5,8 @@ let handler = async (m, { conn }) => {
 
   let list = [
     {
-      displayName: "Cristian Escobar",
-      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Dev Criss 🇦🇱\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú 🇵🇪;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
-    },
+      displayName: "Chinchu dzn",
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Chinchu dzn \nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem4.ADR:;;Argentina 🇦🇷;;;; },
     {
       displayName: "Jota",
       vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Colombia 🇨🇴;;;;\nitem4.X-ABLabel:País\nEND:VCARD`

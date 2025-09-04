8ce76f3d9c009762febd82f4d03c152f69c1b47e
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 22:31:21 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -78,26 +78,25 @@ if (chat.welcome && m.messageStubType == 32) {
   const groupDesc = groupMetadata.desc || 'sin descripción'
 
   const msgsBye = [
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗧𝗨 𝗔𝗨𝗦𝗘𝗡𝗖𝗜𝗔 𝗙𝗨𝗘 𝗖𝗢𝗠𝗢 𝗨𝗡 𝗤𝗟𝗢,* 
-*┊𝗖𝗢𝗡 𝗢𝗟𝗢𝗥 𝗔 𝗠𝗥𝗗!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗔𝗟𝗚𝗨𝗜𝗘𝗡 𝗠𝗘𝗡𝗢𝗦, 𝗤𝗨𝗜𝗘𝗡 𝗧𝗘 𝗥𝗘𝗖𝗨𝗘𝗥𝗗𝗘* 
-*┊𝗦𝗘𝗥𝗔 𝗣𝗢𝗥 𝗟𝗔𝗦𝗧𝗜𝗠𝗔, 𝗔𝗗𝗜𝗢𝗦!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗧𝗨 𝗗𝗘𝗦𝗣𝗘𝗗𝗜𝗗𝗔 𝗡𝗢𝗦 𝗛𝗔𝗥𝗔 𝗟𝗟𝗢𝗥𝗔𝗥,* 
-*┊𝗗𝗘 𝗟𝗔 𝗩𝗘𝗥𝗚𝗨𝗘𝗡𝗭𝗔 𝗤𝗨𝗘 𝗗𝗔𝗕𝗔𝗦!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`,
-    `*╭┈┈┈┈┈┈┈┈┈┈┈┈┈≫*
-*┊* *${taguser}*
-*┊𝗗𝗘𝗝𝗢 𝗗𝗘 𝗢𝗟𝗘𝗥 𝗔 𝗠𝗥𝗗,* 
-*┊𝗛𝗔𝗦𝗧𝗔 𝗤𝗨𝗘 𝗧𝗘 𝗟𝗔𝗥𝗚𝗔𝗦𝗧𝗘!!* 👿
-*╰┈┈┈┈┈┈┈┈┈┈┈┈┈≫*`
+    `╭───────👾───────╮
+     🚪 SE FUÉ 🚪
+    ${taguser}
+╰───────👾───────╯
+
+🛑 No aguantó la presión  
+😂 Otro caído en el intento  
+🔥 El grupo sigue... pero sin ti  
+
+「 Gracias por nada, campeón 🐌 」`,
+    `╔════ ⚰️ ════╗
+   👋 ADIÓS NOOB 👋
+╚════ ⚰️ ════╝
+
+${taguser} decidió  
+➤ Abandonar el grupo 🎮  
+➤ Muy malo era  
+
+🪦 Que le vaya bien por la sombrita...`
   ]
 
   let bye = chat.sBye

653612d12885be5a67323b54fcae46ce9b82fe28
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-04 18:23:01 -0500

    Actualizar descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -4,7 +4,8 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, usedPrefix, command, args }) => {
     if (!args[0]) {
-        return await conn.reply(m.chat, `*${emojis} Ingresa una url válida de TikTok.*`, m);
+        return await conn.reply(m.chat, `*${emojis} Ingresa una url válida de TikTok.*
+> ejemplo: https://vt.tiktok.com/ZSA7hA582/`, m);
     }
 
     if (!args[0].match(/(https?:\/\/)?(www\.)?(vm\.|vt\.)?tiktok\.com\//)) {

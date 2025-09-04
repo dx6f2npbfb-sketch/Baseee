9f0daed88e03b1c9cb342fd42534e5bffe5ed388
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-03 19:07:51 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
     await m.react('🕷️')
 
-    const img = 'https://files.catbox.moe/twwhc9.jpg'
+    const img = 'https://files.catbox.moe/twwhc9.jpg
 
 let tags = {};
 let emojis = {

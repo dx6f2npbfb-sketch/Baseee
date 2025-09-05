6b274031d3cbfc567e9790847cb34c9e87c5651f
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-04 23:54:44 -0500

    Actualizar herramientas-tourl.js

--- plugins/herramientas-tourl.js
+++ plugins/herramientas-tourl.js
@@ -6,7 +6,7 @@ import { fileTypeFromBuffer } from "file-type";
 const handler = async (m, { conn }) => {
 let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || "";
-  if (!mime) return conn.reply(m.chat, `*${emojis} Responde a una imagen, vídeo o gif.*`, m, rcanal);
+  if (!mime) return conn.reply(m.chat, `*${emojis} Responde a una imagen, vídeo o gif.*`, m);
   let media = await q.download();
 let link = await catbox(media);
   let caption = `*Tools - Tourl*

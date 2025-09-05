57a6a23250178b985d987097d93527292ea383b2
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-04 23:12:35 -0500

    Actualizar nsfw-penetrar.js

--- plugins/nsfw-penetrar.js
+++ plugins/nsfw-penetrar.js
@@ -11,7 +11,13 @@ let handler = async (m, { conn, command, text }) => {
 *Le ha metido el pene a ${text || userName}* con todo y condón hasta quedar seco, has dicho "por favor más duroooooo!, ahhhhhhh, ahhhhhh, hazme un hijo que sea igual de pitudo que tú!" mientras te penetraba y luego te ha dejado en silla de ruedas!
 
 *${text || userName}* 
-🔥 *YA TE HAN PENETRADO!*`;
+🔥 *YA TE HAN PENETRADO!*`,
+`*QUE CREES PUTITX* 
+
+*Te descuidaste y te han metido todo el 🍆 dentro ${texto || userName}, mientras gemias cómo puta y pedías que te dieran más y más*
+
+*${texto || userName}*
+🥵*ya estás clavadx, solo te queda menearte`;
 
     // Envía la respuesta al chat
     conn.reply(m.chat, responseMessage, null, { mentions: [user] });

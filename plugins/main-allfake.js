54a60ff02685279f1597e95c31c24de355e3ca29
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-05 15:28:05 -0500

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -45,9 +45,9 @@ global.error = '✖️'
 //EMOJIS PREDETERMINADOS
 global.emoji = '🕷️'
 global.emoji2 = '🫟'
-global.emoji3 = '👾'
-global.emoji4 = '🫧'
-global.emoji5 = '☁️'
+global.emoji3 = '✨'
+global.emoji4 = '🕸️'
+global.emoji5 = '🍄'
 global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
 
 //EMOJIS INFORMATIVOS

bd5a9a082e2078ad8902f4dab0e96669b8663b08
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-05 22:08:37 -0500

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -6,21 +6,21 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
-} else return conn.reply(m.chat, `*${emojis} Agrega un texto.*`, m);
-if (!text) return conn.reply(m.chat, `*${emojis} Agrega un texto.*`, m);
+} else return conn.reply(m.chat, `*${xsticker} Por favor, ingresa o responde a un texto para realizar tu sticker.*`, m, rcanal);
+if (!text) return conn.reply(m.chat, `*${xsticker} Agrega un texto.*`, m);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 40) return conn.reply(m.chat, '*⚠️ El texto no puede tener mas de 30 caracteres* \n> acaso vas a escribir tu testamento hijx?', m);
+if (mishi.length > 40) return conn.reply(m.chat, '*⚠️ El texto no puede tener mas de 30 caracteres*', m);
 const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 const nombre = await conn.getName(who)
-const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": usname, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
+const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
 const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
 const buffer = Buffer.from(json.data.result.image, 'base64');
 let stiker = await sticker(buffer, false, global.packname, global.author);
 if (stiker) return conn.sendFile(m.chat, stiker, 'error.webp', '', m);
 }
-handler.help = ['qc'];
+handler.help = ['qc *<txt>*'];
 handler.tags = ['sticker'];
 handler.command = /^(qc)$/i;
 

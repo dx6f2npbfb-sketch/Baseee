bc1173f38026841bf4f56b07c37789fe74178873
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-05 21:41:22 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,12 +1,25 @@
-const handler = async (m, { conn }) => {
+import fetch from 'node-fetch'
 
+let handler = async (m, { conn }) => {
+  await m.react(emojis);
 
-  conn.sendMessage(m.chat, {
-text: `Hola ${club} aquí tienes el contacto de mi dueño\nJota🐼: +573155227977\n> escribe si quieres info del bot`,
-}, { quoted: m });
+  let list = [
+    {
+      displayName: "Chinchu",
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=5493855789747:5493855789747\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;; Argentina 🇦🇷;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+    }
+  ];
+
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: `${list.length} Contactos`,
+      contacts: list
+    }
+  }, { quoted: estilo });
 };
 
+handler.help = ['creador', 'dueño'];
 handler.tags = ['info'];
-handler.help = ['comprar'];
-handler.command = ['Dueño', 'owner'];
+handler.command = /^(owner|creador|creator|dueño|desarrollador)$/i;
+
 export default handler;

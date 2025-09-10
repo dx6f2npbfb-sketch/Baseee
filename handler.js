1f8d13c001ba26de664bc29b56a9cb40c63c499d
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-09 22:35:40 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -622,7 +622,7 @@ unreg: `*${emoji} \`${botname}\` te avisa que no te encuentras registrado para u
 > Ejemplo: .reg ${namebot}.21`,
 restrict: `*${emoji} Esta característica esta desactivada.*`
 }[type];
-if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))}
+if (msg) return conn.reply(m.chat, msg, m).then(_ => m.react('✖️'))}
 
 let file = global.__filename(import.meta.url, true)
 watchFile(file, async () => {

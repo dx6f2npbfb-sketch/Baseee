7489188687ceaad3ebdb16366a1ce4afd49f52ae
Jotaahrz <rodriguezjota90@gmail.com>
2025-09-10 18:36:21 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 
     await m.react('🕷️')
 
-    const vid = 'https://files.catbox.moe/sgeolq.mp4'
+    const vid = 'https://files.catbox.moe/wg4skm.mp4'
 
 let tags = {};
 let emojis = {

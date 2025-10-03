1e754e893a62b20a5d5d7d6f1eca5d7f7e172d83
Jotaahrz <rodriguezjota90@gmail.com>
2025-10-03 15:30:52 -0500

    Actualizar ventas-setpago.js

--- plugins/ventas-setpago.js
+++ plugins/ventas-setpago.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (text) {
         global.db.data.chats[m.chat].setpago = text
         conn.reply(m.chat, '💰𝙇𝙖𝙨 𝙞𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞𝙤𝙣𝙚𝙨 𝙙𝙚 𝙥𝙖𝙜𝙤 𝙝𝙖𝙣 𝙨𝙞𝙙𝙤 𝙖𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙙𝙖𝙨.\n\n> MxdeBot', m)
-    } else throw `🕷️𝙀𝙨𝙘𝙧𝙞𝙗𝙚 𝙡𝙖𝙨 𝙞𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞𝙤𝙣𝙚𝙨 𝙙𝙚 𝙥𝙖𝙜𝙤 𝙦𝙪𝙚 𝙙𝙚𝙨𝙚𝙖𝙨 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧, 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n.
+    } else throw `🕷️𝙀𝙨𝙘𝙧𝙞𝙗𝙚 𝙡𝙖𝙨 𝙞𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞𝙤𝙣𝙚𝙨 𝙙𝙚 𝙥𝙖𝙜𝙤 𝙦𝙪𝙚 𝙙𝙚𝙨𝙚𝙖𝙨 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧, 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n
 > .𝙨𝙚𝙩𝙥𝙖𝙜𝙤 𝘾𝙪𝙚𝙣𝙩𝙖: 1234 5678 9012 3456 𝘽𝙖𝙣𝙘𝙤: 𝘽𝘽𝙑𝘼\n\n> YancitoBot`
 }
 

c6aea1502d47ea30f0de0bc1bce8b1d663615ac5
Jotaahrz <rodriguezjota90@gmail.com>
2025-10-03 15:21:33 -0500

    Actualizar ventas-stock.js

--- plugins/ventas-stock.js
+++ plugins/ventas-stock.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
         let stock = chat.setstock;
         await conn.reply(m.chat, stock, m);
     } else {
-        m.reply(`🥷 𝙉𝙤 𝙨𝙚 𝙝𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤 𝙪𝙣 𝙘𝙖𝙩á𝙡𝙤𝙜𝙤 𝙥𝙖𝙧𝙖 𝙎𝙩𝙤𝙘𝙠, 𝙪𝙩𝙞𝙡𝙞𝙯𝙖 .𝙨𝙚𝙩𝙨𝙩𝙤𝙘𝙠 𝙥𝙖𝙧𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙪𝙣𝙤.\n\n> YamcitoBot`);
+        m.reply(`🥷 𝙉𝙤 𝙨𝙚 𝙝𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤 𝙪𝙣 𝙘𝙖𝙩á𝙡𝙤𝙜𝙤 𝙥𝙖𝙧𝙖 𝙎𝙩𝙤𝙘𝙠, 𝙪𝙩𝙞𝙡𝙞𝙯𝙖 .𝙨𝙚𝙩𝙨𝙩𝙤𝙘𝙠 𝙥𝙖𝙧𝙖 𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙚𝙧 𝙪𝙣𝙤.\n\n> YancitoBot`);
     }
 }
 handler.command = ['stock'];

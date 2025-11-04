a7cb5f53ed4d4b532f38f964c65af546350e596b
Jotaahrz <rodriguezjota90@gmail.com>
2025-11-04 11:50:02 -0500

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -1,27 +1,57 @@
-import { sticker } from '../lib/sticker.js';
-import axios from 'axios';
-const handler = async (m, {conn, args, usedPrefix, command}) => {
-let text
-if (args.length >= 1) {
-text = args.slice(0).join(" ");
-} else if (m.quoted && m.quoted.text) {
-text = m.quoted.text;
-} else return conn.reply(m.chat, `*${xsticker} Por favor, ingresa o responde a un texto para realizar tu sticker.*`, m);
-if (!text) return conn.reply(m.chat, `*${xsticker} Agrega un texto.*`, m);
-const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
-const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
-const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 40) return conn.reply(m.chat, '*⚠️ El texto no puede tener mas de 30 caracteres*', m);
-const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-const nombre = await conn.getName(who)
-const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
-const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
-const buffer = Buffer.from(json.data.result.image, 'base64');
-let stiker = await sticker(buffer, false, global.packname, global.author);
-if (stiker) return conn.sendFile(m.chat, stiker, 'error.webp', '', m);
-}
-handler.help = ['qc *<txt>*'];
-handler.tags = ['sticker'];
-handler.command = /^(qc)$/i;
+import { sticker } from '../lib/sticker.js'
+import axios from 'axios'
 
-export default handler;
+const handler = async (m, { conn, args }) => {
+    let text;
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ");
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text;
+    } else throw "🎀 *Ingrese un texto para realizar su stiker quotly.*\n\n> Ejemplo: .qc Hello world.";
+   if (!text) return conn.reply(m.chat, '🎀 *Ingrese un texto para realizar su stiker quotly.*\n\n> Ejemplo: .qc Hello world.', m)
+   if (text.length > 30) return conn.reply(m.chat, '> Máximo 3 carácteres, no es una biblia hijo.', m)
+
+    const randomColor = ['#000000'];
+
+    const apiColor = randomColor[Math.floor(Math.random() * randomColor.length)];
+
+    const pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/320b066dc81928b782c7b.png');
+
+    const obj = {
+        "type": "quote",
+        "format": "png",
+        "backgroundColor": apiColor,
+        "width": 512,
+        "height": 768,
+        "scale": 2,
+        "messages": [{
+            "entities": [],
+            "avatar": true,
+            "from": {
+                "id": 1,
+                "name": m.name,
+                "photo": {
+                    "url": pp
+                }
+            },
+            "text": text,
+            "replyMessage": {}
+        }]
+    };
+
+    const json = await axios.post('https://btzqc.betabotz.eu.org/generate', obj, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const buffer = Buffer.from(json.data.result.image, 'base64');
+    const stiker = await sticker(buffer, false, global.stickpack, global.stickauth);
+    if (stiker) return conn.sendFile(m.chat, stiker, 'Quotely.webp', '', m);
+}
+
+handler.help = ['quotly *<texto>*']
+handler.tags = ['sticker']
+handler.command = ['quotly', 'qc']
+
+export default handler
